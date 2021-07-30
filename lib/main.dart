004d3300b6ac1ac27526926314328d5b15c7dd1e
Soikat77 <soikatalam26@gmail.com>
2021-07-30 10:26:27 +0600

    Scafold App Bar and Body Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('My First App'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material Design'),
+        ),
+        body: Center(
+          child: Text('Hello, Flutter!'),
+        ),
+      ),
     );
   }
 }
