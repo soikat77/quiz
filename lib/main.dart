9f5f7da80e74280440f698616f04793fd67cef91
Soikat77 <soikatalam26@gmail.com>
2021-08-01 19:48:20 +0600

    Question and Answer Added

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,40 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer Choosen!");
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What is your favorite color?',
+      'What is your favorite animal',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Material Design'),
         ),
         body: Column(
-          children: [
-            Text('Question 1'),
+          children: <Widget>[
+            // Question will set here
+            Text(questions[0]),
+            // Answer will set here
             ElevatedButton(
-              onPressed: null,
-              child: Text('Elevated Button'),
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2'),
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('Answer 3');
+              },
             ),
           ],
         ),
